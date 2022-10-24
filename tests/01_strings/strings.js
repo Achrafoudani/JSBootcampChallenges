8f881104eaddef3a6b8bdbf9d68afe7792b9372a
Achrafoudani <99147282+Achrafoudani@users.noreply.github.com>
2022-10-25 00:18:41 +0200

    Add Solutions for all strings exercises

--- tests/01_strings/strings.js
+++ tests/01_strings/strings.js
@@ -1,21 +1,36 @@
 /* eslint-disable no-unused-vars */
 
 function doesInclude(str, letter) {
-
+  if (str.includes(letter)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isPalindrome(str) {
-
+  let isPlaindrome = str.split("").reverse().join("") === str;
+  return isPlaindrome;
 }
 
 function cap(str, letter) {
-
+  if (str.indexOf(letter) !== -1) {
+    let char = str.indexOf(letter);
+    return str[char + 1].toUpperCase();
+  } else {
+    return "sorry not found";
+  }
 }
 
 function firstCharacter(str1, str2) {
-
+  const isFirstCharachter = str1[0].toLowerCase() === str2[0].toLowerCase();
+  return isFirstCharachter;
 }
 
 module.exports = {
-  cap, firstCharacter, doesInclude, isPalindrome,
+  cap,
+  firstCharacter,
+  doesInclude,
+  isPalindrome,
 };
+
